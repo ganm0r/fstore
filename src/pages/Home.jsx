1920a0ec5a1dd1af703ea4450df4a16b4ac1b714
ganm0r <gndhrvmv@gmail.com>
2022-11-11 23:21:01 +0530

    :recycle: Refactor categories into sections with colors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react';
+
 import { Card } from '../components/Card';
+import { fstoreTheme } from '../theme';
+
 import axios from '../helpers/apiClient';
 
-const CATEGORIES = [
-    "men's clothing",
-    "women's clothing",
-    "electronics",
-    "jewelery",
-];
+const SECTIONS = [
+    {
+        category: "men's clothing",
+        bgColor: fstoreTheme.colors.white,
+        fontColor: fstoreTheme.colors.black,
+    },
+    {
+        category: "women's clothing",
+        bgColor: fstoreTheme.colors.pink,
+        fontColor: fstoreTheme.colors.white,
+    },
+    {
+        category: "electronics",
+        bgColor: fstoreTheme.colors.white,
+        fontColor: fstoreTheme.colors.black,
+    },
+    {
+        category: "jewelery",
+        bgColor: fstoreTheme.colors.pink,
+        fontColor: fstoreTheme.colors.white,
+    },
+]
 
 const Home = () => {
     const [products, setProducts] = useState([]);
@@ -19,10 +38,10 @@ const Home = () => {
     
     return (
         <div>
-            {CATEGORIES.map(category => (
-                <div key={category} style={{ display: 'flex', overflowX: 'scroll', overflowY: 'hidden', scrollbarWidth: 'thin', height: "90vh" }}>
-                    {products && products.map(product => product.category === category && (
-                        <Card key={product.title} image={product.image} title={product.title.split(' ').slice(0, 3).join(' ')} price={product.price} />
+            {SECTIONS.map(section => (
+                <div key={section.category} style={{ display: 'flex', overflowX: 'scroll', overflowY: 'hidden', scrollbarWidth: 'thin', height: "100vh" }}>
+                    {products && products.map(product => product.category === section.category && (
+                        <Card key={product.title} image={product.image} title={product.title.split(' ').slice(0, 3).join(' ')} price={product.price} bgColor={section.bgColor} fontColor={section.fontColor} />
                     ))}
                 </div>
             ))}
