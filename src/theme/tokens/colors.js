94de376f7bb2cab15cf3048cac1b2a43895b06f4
ganm0r <gndhrvmv@gmail.com>
2022-11-11 23:20:27 +0530

    :lipstick: Update accent colors

--- src/theme/tokens/colors.js
+++ src/theme/tokens/colors.js
@@ -1,12 +1,12 @@
 const colors = {
     /** #FAFAFA */
-    white: '#FAFAFA',
+    white: '#E6E6E6',
 
     /** #1B1B1B */
     black: '#1B1B1B',
 
-    /** #CAAFA8 */
-    pink: '#CAAFA8',
+    /** #EA4E7D */
+    pink: '#EA4E7D',
 };
 
 export default colors;
