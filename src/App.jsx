642eef9056a4a3d16e5d7df595df6e202e068ebb
ganm0r <gndhrvmv@gmail.com>
2022-11-12 11:20:05 +0530

    :truck: Add routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
+import { Search } from './pages/Search';
 
-const App = () => (
-    <div>
-        <Navbar />
-        <Home />
-    </div>
-);
+import axios from './helpers/apiClient';
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("/products")
+            .then(res => setProducts(res.data))
+    }, []);
+
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home products={products} />} />
+                    <Route path='/search' element={<Search products={products} />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+};
 
 export default App;
